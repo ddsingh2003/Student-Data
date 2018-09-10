7ad4790bd31252e6c5dd1287d814e13c0280bb7e
ddsingh2003 <43127916+ddsingh2003@users.noreply.github.com>
2018-09-10 11:17:10 +0530

    Update 1-CastDemo.java

--- 1-CastDemo.java
+++ 1-CastDemo.java
@@ -2,12 +2,14 @@
 
 class CastDemo
 {
+	
 	     public static void main(String[] args)
 	     {
    		float var1 = 10;  //implicit conversion
 		int var2 = (int)10.7;   //Explicit Conversion
 
 		System.out.println("The value of var1 is"+var1);
+		System.out.println();
 		System.out.println("The value of var2 is"+var2);
      	     }
 }
