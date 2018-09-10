cc662d2482532f9e5094a0462cd780650ab2c8df
ddsingh2003 <43127916+ddsingh2003@users.noreply.github.com>
2018-09-10 11:13:38 +0530

    Add files via upload

---
+++ 1-CastDemo.java
@@ -0,0 +1,14 @@
+//This demo is for Implicit Conversion and Explicit Conversion.
+
+class CastDemo
+{
+	     public static void main(String[] args)
+	     {
+   		float var1 = 10;  //implicit conversion
+		int var2 = (int)10.7;   //Explicit Conversion
+
+		System.out.println("The value of var1 is"+var1);
+		System.out.println("The value of var2 is"+var2);
+     	     }
+}
+
